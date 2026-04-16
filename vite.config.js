97f78ca25991e9a87ba3c40dab02a432a51bf0a1
Andrey <byketch@yandex.ru>
2026-04-16 18:09:23 +0300

    fix: оптимизация графики

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,9 @@
-import { defineConfig } from 'vite'
-import viteImagemin from 'vite-plugin-imagemin'
+import { defineConfig } from 'vite';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 import path from 'path';
 
 export default defineConfig({
   plugins: [
-    viteImagemin({
-      pngquant: {
-        quality: [0.7, 0.8], 
-        speed: 4,
-      },
-      mozjpeg: {
-        quality: 75,
-      },
-      webp: {
-        quality: 75,
-      },
-    }),
     createSvgIconsPlugin({
       iconDirs: [path.resolve(process.cwd(), 'src/icons')],
       symbolId: 'icon-[dir]-[name]',
